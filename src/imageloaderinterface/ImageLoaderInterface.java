dc3ee400a2ec66ac84f4aa46808e9b41466f79d9
Camille <camil@PC_Camille>
2025-06-04 10:32:36 +0200

    actualise the move

--- src/imageloaderinterface/ImageLoaderInterface.java
+++ src/imageloaderinterface/ImageLoaderInterface.java
@@ -2,6 +2,7 @@ package imageloaderinterface;
 
 import Exeptions.ResourcesFileErrorException;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
@@ -10,6 +11,7 @@ import javafx.scene.input.DragEvent;
 import javafx.stage.FileChooser;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.beans.property.BooleanProperty;
@@ -17,6 +19,7 @@ import javafx.beans.property.SimpleBooleanProperty;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.VBox;
+import javax.imageio.ImageIO;
 
 
 public class ImageLoaderInterface extends VBox {
@@ -98,6 +101,17 @@ public class ImageLoaderInterface extends VBox {
                  Image image = new Image("file:" + imageFile.getAbsolutePath());
                            preview.setImage(image);
                            image_out = SwingFXUtils.fromFXImage(image, null);
-        }         
-         
+        }
+
+        public void loadImage(BufferedImage bufferedImage) {
+                if (bufferedImage != null) {
+                        // Convert BufferedImage to JavaFX Image
+                        Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+                        preview.setImage(image);
+                        image_out = bufferedImage;
+                } else {
+                        System.err.println("Error: BufferedImage is null");
+                }
+        }
+
 }
